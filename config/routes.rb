f793b85069ef4f791c5390959a2f343b24639684
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-04-30 21:00:50 +0800

    [Feature 1] config admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   namespace :users do
     resources :home
   end
+
+  devise_for :users, as: 'admin', controllers: { sessions: 'admin/sessions', registrations: 'admin/registrations' }
+  namespace :admin do
+    resources :home
+  end
 end
