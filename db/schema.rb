5577d3bcf92bee34ae076c270e0321c43ac9d39e
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-05-02 18:52:32 +0800

    [Feature 6] Create user_address model and table with genre, name, street_address, phone_number, remark and is_default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_30_171049) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_104701) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -41,6 +41,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_30_171049) do
   create_table "address_regions", charset: "utf8mb4", force: :cascade do |t|
     t.string "code"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_addresses", charset: "utf8mb4", force: :cascade do |t|
+    t.integer "genre", default: 0
+    t.string "name"
+    t.string "street_address"
+    t.string "phone_number"
+    t.string "remark"
+    t.boolean "is_default"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
