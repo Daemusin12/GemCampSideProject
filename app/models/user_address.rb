93d2c0484829f69a6e396f15bede071904a0f6fa
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-05-02 19:07:19 +0800

    [Feature 6] Create Max 5 addresses per user validation method, add users to user_address association

--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -6,6 +6,16 @@ class UserAddress < ApplicationRecord
     types: %i[voip mobile],
     countries: [:ph]
   }
+  validate :address_per_user, on: :create
+
+  belongs_to :users, class_name: 'User', foreign_key: 'users_id'
 
   enum genre: { home: 0, office: 1 }
+
+  def address_per_user
+    address_count = UserAddress.where(users_id: (self.users_id)).count
+    if address_count >= 5
+      self.errors.add(:users_id, "max address per user reached")
+    end
+  end
 end
