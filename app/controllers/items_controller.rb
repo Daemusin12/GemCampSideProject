0b42eb6ca16ad75d64af4a0b42c02a95d6812793
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-05-11 12:32:39 +0800

    [Feature 10] Create ITEM crud

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,46 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_admin_user!
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @item.update(item_params)
+      redirect_to items_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :image, :quantity, :minimum_bets, :batch_count,
+                                 :online_at, :offline_at, :start_at, :status)
+  end
+
+end
